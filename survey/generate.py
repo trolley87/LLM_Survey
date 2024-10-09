7035945c201a50a6e265851159c93705387560cc
Gabriel Laverghetta <glaverghetta@usf.edu>
2024-10-09 14:23:24 -0400

    Restructuring code and datasets

---
+++ survey/generate.py
@@ -0,0 +1,77 @@
+from prompt import *
+import pandas as pd
+
+def convert5():
+    pass
+
+def convert41():
+    pass
+
+def convert9():
+    pass
+
+def convert26():
+    pass
+
+def convert14():
+    pass
+
+def convert15():
+    pass
+
+def convert21():
+    pass
+
+def convert29():
+    pass
+
+def convert38():
+    pass
+
+def convert39():
+    pass
+
+def convert24():
+    pass
+
+def convert27():
+    krone_to_dollar = 0.095 # as of 9/27/2024
+    df = pd.read_csv("datasets/27.csv")
+    df["Prompt"] = "" # add new Prompt column, defaults to empty string
+    with open("output/27.csv", "w", newline = '') as outfile:
+        for i in range(0, len(df.index), 3):
+            row1 = df.iloc[i]
+            row2 = df.iloc[i+1]
+            price1Str = str(int(row1["Cost"]) * krone_to_dollar)
+            price2Str = str(int(row2["Cost"]) * krone_to_dollar)
+            prompt = prompt_templates[27].format(price1=price1Str,  price2=price2Str,
+                                                 capacity1=row1["Capacity"], capacity2=row2["Capacity"],
+                                                 energy1=row1["Energy"], energy2=row2["Energy"],
+                                                 safety1=row1["Safety"], safety2=row2["Safety"])
+            df.at[i+2,"Prompt"] = prompt
+        df.to_csv(outfile, index = False)
+
+def convert22():
+    pass
+
+def convert23():
+    pass
+
+def main():
+    convert5()
+    convert41()
+    convert9()
+    convert26()
+    convert14()
+    convert15()
+    convert21()
+    convert29()
+    convert38()
+    convert39()
+    convert24()
+    convert27()
+    convert22()
+    convert23()
+
+if __name__ == "__main__":
+    main()
---
+++ survey/prompt.py
@@ -0,0 +1,45 @@
+''' Papers:
+Mode choice:
+5. Ride to the hills, ride to your school
+41. Travel to school mode choice
+Pay for food
+9. Willingness to pay for sustainable beer
+26. Consumer attitudes toward pangasius and tilapia
+Pay for goods
+14. 
+15.
+Pay for environment
+21. Economic valuation of urban greening
+29. Investigating peoples preferennces for car-free city centers
+Pay for energy
+38. Consumer willingness to pay for renewable and nuclear energy
+39. Preferences for improved cook stoves
+Pay for car
+24. 
+27. Role of fuel cost info in car sales
+Health
+22. Population preferencesfor breast cancer screening
+23. Preferences for HIV testing services
+'''
+
+prompt_templates = {
+    5: "",
+    41: "",
+    9: "",
+    26: "",
+    14: "",
+    15: "",
+    21: "",
+    29: "",
+    38: "",
+    39: "",
+    24: "",
+    27: "Suppose you are a person deciding on a new car to purchase. You have two cars to choose from. \
+- Car 1 costs {price1} dollars, and Car 2 costs {price2} dollars. \
+- Car 1 has {capacity1} liters of storage space, and Car 2 has {capacity2} liters of storage space. \
+- Car 1 uses {energy1} liters of gasoline per 10 km of travel, and Car 2 uses {energy2} liters of gasoline per 10 km of travel. \
+- Car 1 achieves a safety score of {safety1}% based on the European New Car Assessment Programme, and Car 2 achieves a safety score of {safety2}% from the same test. \
+Respond with 1 if you would purchase Car 1, 2 if you would purchase Car 2, or 3 if you would purchase neither car.",
+    22: "",
+    23: ""
+ }
